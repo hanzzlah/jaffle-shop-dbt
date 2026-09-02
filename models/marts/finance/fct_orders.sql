67753efbae4e9a78d83a5a8a7982876c8df914a1
Hanzlah Yasir <hanzlahyasir@gmail.com>
2026-09-02 07:42:07 +0000

    refactored staging models with codegen

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,9 +1,9 @@
 select 
-    o.order_id as order_id,
-    o.customer_id as customer_id,
+    o.id as order_id,
+    o.user_id as user_id,
     p.amount as amount 
 from 
     {{ ref('stg_jaffle_shop__orders') }} as o
     left join {{ ref('stg_stripe__payments') }} as p
-    on o.order_id = p.orderid
+    on o.id = p.orderid
 where p.status <> 'fail'
